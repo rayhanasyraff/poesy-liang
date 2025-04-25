9a19aca7e0c04dc711cef999a909c3614026bf46
rayhanasyraff <rayhanasyraffhjamran@gmail.com>
2025-04-25 10:12:09 +0800

    Repositioning and decoupling headers

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Header from "./header";
+
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center">
-        <h1>Poesy Liang</h1>
+    <div>
+      <Header />
     </div>
   );
 }
