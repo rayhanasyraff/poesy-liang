e814e19df2076973a566e1d43a1e522655283104
rayhanasyraff <rayhanasyraffhjamran@gmail.com>
2025-04-25 09:52:44 +0800

    Changing headers

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <div>
         <h1>Poesy Liang</h1>
-      </div>
     </div>
   );
 }
